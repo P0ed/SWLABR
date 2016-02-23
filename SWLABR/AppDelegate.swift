3fd8abaf1446556ce893b69a7f3df5315645d646
Konstantin Sukharev <poed@me.com>
2016-02-23 12:01:21 +0300

    Add files

---
+++ SWLABR/AppDelegate.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    }
+}
