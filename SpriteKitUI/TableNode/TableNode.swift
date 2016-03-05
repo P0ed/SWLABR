0f93ad9c7bc85ffd582b97f35b3256446264e932
Настя <juyka@yahoo.com>
2016-03-05 13:50:32 +0300

    add TableNode

--- SpriteKitUI/TableNode/TableNode.swift
+++ SpriteKitUI/TableNode/TableNode.swift
@@ -9,6 +9,39 @@
 import Foundation
 import SpriteKit
 
-class TableNode: SKNode {
+class TableNode<CellType: SKNode, ItemType>: SKNode {
+    var cellNodes: [CellType] = []
     
+    let model: TableViewModel<ItemType>
+    
+    let cellHeight: Float = 40
+    
+    let createCell: () -> CellType
+    let updateCell: (CellType, ItemType) -> ()
+    
+    init(model: TableViewModel<ItemType>, createCell: () -> CellType, updateCell: (CellType, ItemType) -> ()) {
+        
+        self.model = model
+        self.createCell = createCell
+        self.updateCell = updateCell
+
+        super.init()
+        updateTable()
+        
+    }
+    
+    func updateTable() {
+        cellNodes = model.model.items.map {
+            item in
+            let cell = createCell()
+            updateCell(cell, item)
+            return cell
+        }
+        
+        layoutTable()
+    }
+    
+    func layoutTable() {
+ 
+    }
 }
